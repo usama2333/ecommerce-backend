2fb04cc685880209c4502a9702e51d0cfd5c18f8
usama2333 <usama.wizz@gmail.com>
2024-12-17 18:03:54 +0500

    Admin and super Admin created

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,15 +2,17 @@ const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middleware/authMiddleware');
 const { verifyAdmin, verifySuperAdmin } = require('../middleware/verifyAdmin');
+const { roles } = require('../models/user');
 
-// Super Admin Dashboard route
-router.get('/super-admin-dashboard', verifyToken, verifySuperAdmin, (req, res) => {
-    res.status(200).json({ message: 'Welcome to the Super Admin Dashboard' });
-});
-
-// Admin Dashboard route (Admin or Super Admin access)
+// Admin dashboard (accessible by both Admin and Super Admin)
 router.get('/dashboard', verifyToken, verifyAdmin, (req, res) => {
-    res.status(200).json({ message: 'Welcome to the Admin Dashboard' });
+    // Check if the user is super admin
+    if (req.user.role === roles.SUPER_ADMIN) {
+        return res.status(200).json({ message: 'Welcome to the Super Admin Dashboard' });
+    }
+    
+    // If not super admin, but admin
+    return res.status(200).json({ message: 'Welcome to the Admin Dashboard' });
 });
 
 module.exports = router;
